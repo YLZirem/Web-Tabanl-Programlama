0c2655555caeff93ec8a16f24dba0eeba41cd6fc
YLZirem <irem03yilmaz@gmail.com>
2024-12-20 11:28:28 +0300

    Update Anket.php

--- Anket.php
+++ Anket.php
@@ -29,3 +29,37 @@ if (isset($_POST['language'])) {
 
 $conn->close();
 ?>
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "anket_veritabani"; // phpMyAdmin'de oluşturduğunuz veritabanı adı
+
+// Veritabanına bağlanma
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Bağlantıyı kontrol etme
+if ($conn->connect_error) {
+    die("Bağlantı başarısız: " . $conn->connect_error);
+}
+
+// Anket sonuçlarını alma
+$query = "SELECT language, COUNT(*) AS count FROM anket_sonuc GROUP BY language";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    echo "<h2>Anket Sonuçları</h2>";
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>" . $row['language'] . ": " . $row['count'] . " oy</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "Henüz oy kullanılmamış.";
+}
+
+$conn->close();
+?>
+
